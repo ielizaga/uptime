4437b1bc408ff3ddf6c50336a00bb8400a55bcb0
Ignacio Elizaga <ielizaga@pivotal.io>
2016-06-19 14:30:44 +0100

    Feature/refactor document backend (#4)
    Several code documentation improvements and PEP8 compliance changes.

--- mongo_agent.py
+++ mongo_agent.py
@@ -1,22 +1,33 @@
-import pymongo
 from pymongo import MongoClient
 
 
 class MongoAgent:
-    """
-    This class provides a MongoDB agent used to query data files where user data is stored.
-    """
 
     db = None
 
     def __init__(self, connection_string, db_name):
+        """
+        This class provides a MongoDB agent used to query data files where user data is stored
+        :param connection_string: Connection details of the MongoDB database
+        :param db_name: Database name
+        """
         client = MongoClient(connection_string)
         self.db = client[db_name]
 
     def get_dashboard_data(self, email):
+        """
+        Retrieves the dashboard data from the dashboard collection in the database
+        :param email: user email whose data will be returned
+        :return:
+        """
         collection = self.db["dashboard"]
         return collection.find_one({"email": email})
 
     def get_metrics_data(self, email):
+        """
+        Retrieves the metrics data from the metrics collection in the database
+        :param email: user email whose data will be returned
+        :return:
+        """
         collection = self.db["metrics"]
         return collection.find_one({"email": email})
