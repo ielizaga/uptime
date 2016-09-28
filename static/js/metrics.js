e4a16e22a9ad317f42a48c7a79276b42a9159de3
Faisal Ali <fai28683@gmail.com>
2016-09-28 07:08:20 -0700

    More Metrics data (#15)
    * More Metrics data
    
    + Pushed code related to assignment by month by agent
    + pushed code related to solved by month by agent
    
    * Fixed some typos
    
    * Fixed few minor bugs with displaying data
    
    * Fixed the KB Analaytics Overlap
    
    + The KB contributed per current month text tends to overlap when all the data starts to show up, this leads to unreadable information
    + The fix the make the chart from bar to pie so that we make use of space effectively ...
    
    * a few minor title adjustments

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -6,6 +6,11 @@ var metric_divs = [
     'metric_week_stats'
 ];
 
+var metric_by_month_divs = [
+    'assigned_by_month',
+    'solved_by_month'
+]
+
 $.getJSON($SCRIPT_ROOT + '/get_metrics_data',
     function(data) {
         console.log(data);
@@ -20,4 +25,28 @@ $.getJSON($SCRIPT_ROOT + '/get_metrics_data',
                 $('#'+metric_div+' tbody').append(rowTemplate);
             }
         });
+
+        $.each(metric_by_month_divs, function(id, metric_by_month_div) {
+            for (var i in data.metrics[metric_by_month_div]) {
+                var rowTemplate = '<tr class="tr-color">' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone">' + data.metrics[metric_by_month_div][i]['Year'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Jan'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Feb'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Mar'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Apr'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['May'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['June'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['July'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Aug'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Sep'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Oct'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Nov'] + '</td>' +
+                    '<td class="tr-color mdl-data-table__cell--non-numeric mdl-cell--hide-tablet mdl-cell--hide-phone text-align-right">' + data.metrics[metric_by_month_div][i]['Dec'] + '</td>' +
+                    '</tr>';
+
+                $('#'+metric_by_month_div+' tbody').append(rowTemplate);
+            }
+        });
+
+
     });
